21773c6bb2eca61289b4628e8252796ec3518aae
UlyssesDB <u.bakolias@gmail.com>
2017-10-10 18:36:36 -0400

    First commit

--- src/inheritance/problem1.js
+++ src/inheritance/problem1.js
@@ -4,8 +4,19 @@ class Shape {
     }
 }
 
-class Rectangle {
-
+class Rectangle extends Shape {
+        constructor(width, height) {
+              super("rectangle");
+              this.width = width;
+              this.height = height;
+          }
+          area() {
+              return this.width * this.height;
+          }
+          perimeter() {
+             return this.width + this.width + this.height + this.height;
+          }
+      
     // A rectangle is a shape
     // Every rectangle has a width and a height
     // Implement the constructor
@@ -13,13 +24,18 @@ class Rectangle {
     // The constructor has two arguments: width and height
 }
 
-class Square {
+class Square extends Rectangle {
     // A square is a rectangle
     // Every square has a width and a height
     // The height and width of a square are always the same
     // Implement the constructor
     // Do not implement the area and perimeter methods. They should be inherited from Rectangle
     // The constructor has one argument
+        constructor(size) {
+            super("square");
+            this.height = size;
+            this.width = size;
+        }
 }
 
 
