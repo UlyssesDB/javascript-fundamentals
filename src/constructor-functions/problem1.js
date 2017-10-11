4e3b4ffc442d9664fcb7da4abce75c63826cc563
UlyssesDB <u.bakolias@gmail.com>
2017-10-11 10:27:12 -0400

    Update

--- src/constructor-functions/problem1.js
+++ src/constructor-functions/problem1.js
@@ -1,5 +1,5 @@
 // Add a function to all arrays called isNotEmpty
-// isNotEmpty returns true is the array is empty, false otherwise
+// isNotEmpty returns true is the array is not empty, false otherwise
 Array.prototype.isNotEmpty = function() {
   if (this.length == 0) {return false;}
   if (this.length >= 0) {return true;}
